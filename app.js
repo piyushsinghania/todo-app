6a508e396b22ac5be69bd24d508e1444d698abb5
Piyush-Singhania <piyushsinghania28@gmail.com>
2021-05-19 23:48:18 +0530

    Adding local storage functionality local storage setup for holding the todos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const list = document.querySelector(".todos");
 const addForm = document.querySelector(".add");
 const search = document.querySelector(".search input");
 
+//! generateTemplate function for generating todo templates
 const generateTemplate = (todo) => {
   const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -21,6 +22,14 @@ addForm.addEventListener("submit", (e) => {
 
   if (todo) {
     generateTemplate(todo);
+
+    //! localStorage setup for adding todos
+    let todos = [];
+    if (localStorage.getItem("todos")) {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   addForm.reset();
@@ -29,10 +38,24 @@ addForm.addEventListener("submit", (e) => {
 //! Event listener for deleting selected todo
 list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
+    //! Removing todo from localStorage
+    let todos = [];
+    todos = JSON.parse(localStorage.getItem("todos"));
+
+    let todoToDelete = e.target.previousElementSibling.innerText;
+
+    todos = todos.filter((todo) => {
+      return todo !== todoToDelete;
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    //! Removing the todo from the DOM
     e.target.parentElement.remove();
   }
 });
 
+//! Function to filterTodos
 const filterTodos = (term) => {
   Array.from(list.children)
     .filter((todo) => !todo.textContent.toLowerCase().includes(term))
@@ -48,3 +71,11 @@ search.addEventListener("keyup", () => {
   const term = search.value.toLowerCase().trim();
   filterTodos(term);
 });
+
+//! Checking for todos in localStorage
+if (localStorage.getItem("todos")) {
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((todo) => {
+    generateTemplate(todo);
+  });
+}
